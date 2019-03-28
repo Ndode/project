ea69430c359b7c7de406d392a2689637e9990bbd
dione <gennyvera2003@yahoo.co.uk>
2019-03-28 15:42:32 -0500

    initial

--- instance.tf
+++ instance.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "centos" {
     ami = "${var.ami}"
     instance_type = "${var.instance_type}"
-    subnet_id = "${var.subnet_id}"
     availability_zone = "eu-west-1c"
     key_name = "${var.key_name}"
     associate_public_ip_address = true
