b30a6d4c0c3aac092143ad4143ebd581a1688267
dione <gennyvera2003@yahoo.co.uk>
2019-03-28 12:25:39 -0500

    initial

--- nat_gateway.tf
+++ nat_gateway.tf
@@ -1,6 +1,6 @@
 resource "aws_nat_gateway" "projectngw" {
   allocation_id = "eipalloc-07ea1a111fdfad2f0"
-  subnet_id = "10.0.1.0/24"
+  subnet_id = "subnet-02bcfab3442a78a27"
 
   tags = {
     Name = "projectngw"
