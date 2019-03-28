977b6c2904b3f2399d0e597f0c3ce96d6d256c23
dione <gennyvera2003@yahoo.co.uk>
2019-03-28 12:08:22 -0500

    initial

--- vpc.tf
+++ vpc.tf
@@ -2,13 +2,8 @@ resource "aws_vpc" "project" {
     cidr_block = "10.0.0.0/16"
     instance_tenancy = "dedicated"
 
+    enable_nat_gateway = true
     tags = {
        Name = "project"
     }
 
-resource "aws_eip" "nat" {
-   count = 1
-
-single_nat_gateway = true
-}
-  }
