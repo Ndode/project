78a86761e2c74cfa94bfb7d059d59685bfbafda5
dione <gennyvera2003@yahoo.co.uk>
2019-03-28 16:24:45 -0500

    initial

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
     ami = "${var.ami}"
-    instance_type = "t2.micro"
+    instance_type = "m5.large"
     key_name = "${var.key_name}"
     subnet_id = "${aws_subnet.pubsubnet.id}"
     associate_public_ip_address = true
