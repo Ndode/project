a59945ab779bdb45d769285c465e5c4417f3a35c
dione <gennyvera2003@yahoo.co.uk>
2019-03-28 11:26:43 -0500

    initial

--- rt.tf
+++ rt.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "pubrt" {
 
   route {
       cidr_block = "0.0.0.0/0"
-      gateway_id = "${var.aws_internet_gateway}"   
+      gateway_id = "${var.gateway_id}"   
   }
   tags = {
      Name = "pubrt" 
