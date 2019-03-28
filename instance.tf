f5502183fa580081c6b600e2f5114668504f9902
dione <gennyvera2003@yahoo.co.uk>
2019-03-28 15:24:16 -0500

    initial

---
+++ instance.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "centos" {
+    ami = "${var.ami}"
+    instance_type = "${var.instance_type}"
+    subnet_id = "${var.subnet_id}"
+    availability_zone = "eu-west-1c"
+    key_name = "${var.key_name}"
+    associate_public_ip_address = true
+    vpc_security_group_ids = ["sg-0f03f147fa2e32910"]
+    
+    tags = {
+        Name = "centos7"
+    }
+}
--- variable.tf
+++ variable.tf
@@ -6,5 +6,20 @@ variable "gateway_id" {
     default = "igw-052633b9a89dd35d7"
 }
   
-
-
+variable "ami"  {
+     default = "ami-000a8866eb40ca83c"
+}
+variable "instance_type" {
+    default = "t2.micro"
+}
+variable "subnet_id" {
+    default = "subnet-02bcfab3442a78a27"
+}
+variable "key_name" {
+     default = "terraform-key"
+    
+}
+variable "count" {
+    default = "1"
+  
+}
