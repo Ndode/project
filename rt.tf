8786cdd746beddbd22ffe155b5337f16782d571b
dione <gennyvera2003@yahoo.co.uk>
2019-03-28 12:33:27 -0500

    initial

--- rt.tf
+++ rt.tf
@@ -8,6 +8,17 @@ resource "aws_route_table" "pubrt" {
   tags = {
      Name = "pubrt" 
   }
+
+resource "aws_route_table" "privrt" {
+    vpc_id = "${var.vpc_id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = "nat-0cbd2db17694a1c4b"
+    }
+  
+}
+
 }
 
 
