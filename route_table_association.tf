6df134dc35269911ccace1e9e34e718d9c47dfbb
dione <gennyvera2003@yahoo.co.uk>
2019-03-28 15:54:21 -0500

    intial

--- route_table_association.tf
+++ route_table_association.tf
@@ -7,3 +7,4 @@ resource "aws_route_table_association" "privrtass" {
 resource "aws_route_table_association" "pubrtass" {
     subnet_id = "subnet-02bcfab3442a78a27"
     route_table_id = "rtb-01f8bcc40824bd74d"
+}
